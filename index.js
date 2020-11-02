951076b7f5234ac04aca74ef3ad2586f466fcc8b
Dolly Parton <laurel.milliken@gmail.com>
2020-11-01 21:57:05 -0700

    11.2 11/5 middleware, POST /reviews

--- index.js
+++ index.js
@@ -4,9 +4,45 @@ const jwt = require('jsonwebtoken')
 
 const db = require('./dbConnectExec.js')
 const config = require('./config.js')
+const auth = require('./middleware/authenticate')
 
 const app = express();
 app.use(express.json())
+
+
+
+app.post("/reviews", auth, async (req,res)=>{
+
+    try{ 
+        var movieFK = req.body.movieFK;
+        var summary = req.body.summary;
+        var rating = req.body.rating;
+    
+        if(!movieFK || !summary || !rating){res.status(400).send("bad request")}
+
+        summary = summary.replace("'","''")
+    
+        // console.log("here is the contact in /reviews",req.contact)
+        // res.send("here is your response")
+
+        let insertQuery = `INSERT INTO Review(Summary, Rating, MovieFK, ContactFK)
+        OUTPUT inserted.ReviewPK, inserted.Summary, inserted.Rating, inserted.MovieFK
+        VALUES('${summary}','${rating}','${movieFK}', ${req.contact.ContactPK})`
+
+        let insertedReview = await db.executeQuery(insertQuery)
+
+        // console.log(insertedReview)
+        res.status(201).send(insertedReview[0])
+    }
+    catch(error){
+        console.log("error in POST /review", error);
+        res.status(500).send()
+    }
+})
+
+app.get('/contacts/me', auth, (req,res)=>{
+    res.send(req.contact)
+})
 
 app.get("/hi",(req,res)=>{
     res.send("hello world")
@@ -59,7 +95,7 @@ app.post("/contacts/login", async (req,res)=>{
     // console.log(token)
 
     //4. save the token in db and send token and user info back to user
-    let setTokenQuery = `UPDATE Contacts
+    let setTokenQuery = `UPDATE Contact
     SET Token = '${token}'
     WHERE ContactPK = ${user.ContactPK}`
 
