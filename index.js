8c32129ea6f87078152dbe2109e6575d21fedc45
Dolly Parton <laurel.milliken@gmail.com>
2020-11-01 23:12:42 -0700

    deployment prep

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
 
 const db = require('./dbConnectExec.js')
 const config = require('./config.js')
 const auth = require('./middleware/authenticate')
 
+//azurewebsites.net, colostate.edu
 const app = express();
 app.use(express.json())
+app.use(cors())
+
 
 
 
@@ -199,4 +203,5 @@ app.get("/movies/:pk", (req, res)=>{
 })
 
 
-app.listen(5000,()=>{console.log("app is running on port 5000")})
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{console.log(`app is running on port ${PORT}`)})
